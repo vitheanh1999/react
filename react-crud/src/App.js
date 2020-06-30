daf1b407bcf0047b0efdafa87df134908922261e
vianh1999bg@gmail.com <0969468213>
2020-06-30 23:05:21 +0700

    a

--- react-crud/src/App.js
+++ react-crud/src/App.js
@@ -1,29 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 import { firebaseConnect } from './firebaseConnect';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from './components/Nav';
+import NoteForm from './components/NoteForm';
+import NoteList from './components/NoteList';
 
-function App() {
-  console.log(firebaseConnect);
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Nav></Nav>
+        <div className="container">
+          <div className="row">
+            <NoteList></NoteList>
+            <NoteForm></NoteForm>
+          </div>
+      </div>
+      </div>
+    );
+  }
 }
 
 export default App;
